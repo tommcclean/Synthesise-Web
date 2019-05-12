5a8344b43d31ebc60b2b357aca67c7b69e28b9c8
Thomas William McClean <spam@tommcclean.me>
2019-05-12 10:36:31 +0100

    Update the clock

--- components/widgets/clock.vue
+++ components/widgets/clock.vue
@@ -1,6 +1,29 @@
 <template>
-  <h1 class="clock title">11:56</h1>
+  <h1 class="clock title">{{ time }}</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      time: null
+    }
+  },
+  mounted() {
+    this.updateClock()
+
+    setInterval(() => {
+      this.updateClock()
+    }, 1000)
+  },
+  methods: {
+    updateClock() {
+      const today = new Date()
+      this.time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+    }
+  }
+}
+</script>
 
 <style>
 .clock.title {
